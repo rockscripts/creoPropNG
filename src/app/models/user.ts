98c973cc713db9d3bdbbf4b537e070482ece5809
LuchoV <lucho.2012.tandil@gmail.com>
2018-08-07 15:11:37 -0300

    cambios lucho

---
+++ src/app/models/user.ts
@@ -0,0 +1,9 @@
+export class Propiedad {
+  private email    :string = '';
+  private pass     :string = '';
+  private nombre   :string = '';
+  private apellido :string = '';
+  private dni      :string = '';
+  private telefono :string = '';
+  private tipoUser :number = '';
+}
--- src/app/providers/user.service.ts
+++ src/app/providers/user.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { ConfigService } from './config.service';
 
 @Injectable({
   providedIn: 'root'
@@ -6,10 +9,26 @@ import { Injectable } from '@angular/core';
 export class UserService {
 
   private userData:any = {};
+  private wsi = 'user/login';
 
-  constructor() { }
+  constructor(
+    private http:   HttpClient,
+    private config: ConfigService
+  ) { }
 
   getUserData(){
     return this.userData;
+  }
+
+  logIn(){
+
+  }
+
+  logOut(){
+
+  }
+
+  create(){
+
   }
 }
