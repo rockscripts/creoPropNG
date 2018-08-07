9bb1cabba94f7e52b77abcceffa71ad2d8b1a5ea
LuchoV <lucho.2012.tandil@gmail.com>
2018-08-07 15:19:45 -0300

    cambios lucho

---
+++ src/app/providers/user.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { ConfigService } from './config.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private userData:any = {
+    "idUser" : null
+  };
+  private wsi = 'user/login';
+
+  constructor(
+    private http:   HttpClient,
+    private config: ConfigService
+  ) { }
+
+  getUserData(){
+    return this.userData;
+  }
+
+  logeado(){
+    return this.userData.idUser != null;
+  }
+
+  logIn(){
+
+  }
+
+  logOut(){
+
+  }
+
+  create(){
+
+  }
+}
