64018d0e0d1cede42e8ca20b534597b0170392cf
otniel acevedo <otniel.a.55@gmail.com>
2018-11-23 20:57:57 -0400

    maqueta del perfil publico

--- src/app/pages/public-profile/public-profile.component.ts
+++ src/app/pages/public-profile/public-profile.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { Perfil } from '../../models/perfil';
+import { ProfileService } from '../../providers/profile.service';
 
 @Component({
   selector: 'app-public-profile',
   templateUrl: './public-profile.component.html',
   styleUrls: ['./public-profile.component.css']
 })
-export class PublicProfileComponent implements OnInit {
+export class PublicProfileComponent implements OnInit, OnDestroy {
+  public profile: Perfil;
+  public showProps: boolean = false;
+  private _onDestroy = new Subject<void>();
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private profileService: ProfileService
+  ) { }
 
   ngOnInit() {
+    this.activatedRoute.params
+      .pipe(takeUntil(this._onDestroy))
+      .subscribe(params => {
+
+        this.profileService.getProfile(params.id)
+          .pipe(takeUntil(this._onDestroy))
+          .subscribe((res: any) => {
+            let profileData = { ...res.data };
+            let inmoData = { ...res.data.inmobiliaria };
+
+            delete profileData.inmobiliaria;
+
+            this.profile = new Perfil(profileData, inmoData);
+          });
+
+      });
+  }
+
+  ngOnDestroy(): void {
+    this._onDestroy.next();
+    this._onDestroy.complete();
   }
 
 }
